313aef056cb9bd97aee70930b261db146b094687
shaikh7862 <shaikhismeazam17@gmail.com>
2018-03-04 04:00:12 +0530

    Initial commit

---
+++ app/src/main/java/com/example/zeus/myapplication/activity/SplashScreen.java
@@ -0,0 +1,61 @@
+package com.example.zeus.myapplication.activity;
+
+/**
+ * Created by zeus on 04-03-2018.
+ */
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.example.zeus.myapplication.R;
+
+public class SplashScreen extends Activity {
+
+    // Splash screen timer
+    private static int SPLASH_TIME_OUT = 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+        //        WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        if (Build.VERSION.SDK_INT >= 21) {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+        }
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(SplashScreen.this, welcmeactivity.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+
+}
---
+++ app/src/main/java/com/example/zeus/myapplication/app/AppConfig.java
@@ -0,0 +1,14 @@
+package com.example.zeus.myapplication.app;
+
+/**
+ * Created by zeus on 03-03-2018.
+ */
+
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://192.168.1.122/android_login_api/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://192.168.1.122/android_login_api/register.php";
+
+}
